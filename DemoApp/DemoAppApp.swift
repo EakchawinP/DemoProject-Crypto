5ba13426e5229e831044ffc4c1b0daf384fe9a35
eakchawin pinngearn <ext.eakchawin.pin@truedigital.com>
2023-01-12 17:48:28 +0700

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Eakchawin Pinngearn on 12/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
